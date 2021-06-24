72dba816f1e4a16f2195cf5e90305c991880b2bc
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-06-24 00:30:04 -0500

    saving files

--- src/CardMaker.js
+++ src/CardMaker.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const CardMaker = () => {
+const CardMaker = ({pokes}) => {
 	return (
-		<div>coming from CardMaker</div>
+		<div className="container">
+			<div className="card-body">
+				<p>{pokes.name}</p>
+				<img src={pokes.url} alt="pokemon sprite" />
+				<p>type</p>
+			</div>
+		</div>
 	)
 }
 
--- src/Request.js
+++ src/Request.js
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import CardMaker from './CardMaker'
+import axios from 'axios'
+
 
 const Request = () => {
+	const [pokes, setPokes] = useState([])
+	useEffect(() => {
+		axios.get("https://pokeapi.co/api/v2/pokemon/")
+			.then(res => {
+				console.log(res)
+				setPokes(res.data.results)
+			})
+			.catch(err => {
+			console.log("something went wrong", err)
+		})
+	},[])
 	return (
-		<CardMaker />
+		<CardMaker pokes={pokes}/>
 	)
 }
 
